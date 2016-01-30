4377852071bbd1bc487c4a26b596b9b44b3f9c44
tgtubbs <tgtubbs@umich.edu>
2016-01-30 17:26:12 -0500

    add coordinates to team data

---
+++ geocoder.py
@@ -0,0 +1,19 @@
+
+from geopy.geocoders import Nominatim
+import pandas
+
+teams = pandas.read_csv('~/Desktop/college_basketball_database/data/teams.txt', sep="\t")
+
+geolocator = Nominatim()
+latitude = []
+longitude = []
+for i in range(len(teams["City, State"])):
+    location = geolocator.geocode(teams["City, State"][i])
+    latitude.append(location.raw["lat"])
+    longitude.append(location.raw["lon"])
+    print(teams["School"][i] + ": " + location.raw["lat"] + ", " + location.raw["lon"])
+teams["latitude"] = latitude
+teams["longitude"] = longitude
+
+# teams.to_csv("/Users/travistubbs/cbb_database/data/teams_test.txt", sep="\t", index=False)
+teams.to_csv("/Users/travistubbs/Desktop/teams_test.txt", sep="\t", index=False)
